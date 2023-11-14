a725503108ef582f3b94ca9feb8aab9f2ed78191
Taylor Reid <taylorsreid@gmail.com>
2023-11-14 11:02:17 -1000

    remove dead code

--- api/models.mjs
+++ api/models.mjs
@@ -1,7 +1,6 @@
 import mongoose from "./mongoose.mjs";
-import { EpisodeSchema, LineSchema } from "./schemas.mjs"
+import { EpisodeSchema } from "./schemas.mjs"
 
 const Episode = mongoose.model('episode', EpisodeSchema)
-// const Line = mongoose.model('line', LineSchema)
 
 export {Episode}
